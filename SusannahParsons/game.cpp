9c26bb20c7566131acc32751e47703c5d130da9a
Susannah Parsons <bushcraftparsons@gmail.com>
2022-12-02 20:42:39 +0000

    Day 2 part 2 completed and learned about enums

--- SusannahParsons/game.cpp
+++ SusannahParsons/game.cpp
@@ -2,10 +2,25 @@
 #include <fstream>
 using namespace std;
 
+enum PLAYSCORE{
+    ROCKSCORE=1,
+    PAPERSCORE=2,
+    SCISSORSSCORE=3
+};
+
+enum PLAYCODE: char{
+    ROCKCODE='A',
+    PAPERCODE='B',
+    SCISSORSCODE='C'
+};
+
 class PlayItem{
     public: int itemScore;
     public: int winItemScore;
     public: string name;
+    public: char winCode;
+    public: char loseCode;
+    public: char drawCode;
     PlayItem(){
     }
 
@@ -22,46 +37,62 @@ class PlayItem{
         }
     }
 
+    char getRiggedItemCode(char code){
+        switch(code){
+            case 'X':
+                return loseCode;
+            case 'Y':
+                return drawCode;
+            case 'Z':
+                return winCode;
+            default:
+                throw invalid_argument("Code doesn't represent anything");
+        }
+    }
+
 };
 
 class Rock : public PlayItem{
     public: Rock(){
-        itemScore = 1;
-        winItemScore = 3;//Rock wins against scissors
+        itemScore = ROCKSCORE;
+        winItemScore = SCISSORSSCORE;//Rock wins against scissors
         name = "Rock";
+        winCode = PAPERCODE;//paper will win against rock
+        loseCode = SCISSORSCODE;//scissors will lose against rock
+        drawCode = ROCKCODE;
     }
 
 };
 
 class Paper : public PlayItem{
     public: Paper(){
-        itemScore = 2;
-        winItemScore = 1;//Paper wins against rock
+        itemScore = PAPERSCORE;
+        winItemScore = ROCKSCORE;//Paper wins against rock
         name = "Paper";
+        winCode = SCISSORSCODE;//scissors will win against paper
+        loseCode = ROCKCODE;//rock will lose against paper
+        drawCode = PAPERCODE;
     }
 };
 
 class Scissors : public PlayItem{
     public: Scissors(){
-        itemScore = 3;
-        winItemScore = 2;//Scissors win against paper
+        itemScore = SCISSORSSCORE;
+        winItemScore = PAPERSCORE;//Scissors win against paper
         name = "Scissors";
+        winCode = ROCKCODE;//rock will win against scissors
+        loseCode = PAPERCODE;//paper will lose against scissors
+        drawCode = SCISSORSCODE;
     }
 };
 
 PlayItem decodePlay(char code){
     switch(code){
-    case 'A':
+    case ROCKCODE:
         return Rock();
-    case 'B':
+    case PAPERCODE:
         return Paper();
-    case 'C':
-        return Scissors();
-    case 'X':
-        return Rock();
-    case 'Y':
-        return Paper();
-    case 'Z':
+    case SCISSORSCODE:
         return Scissors();
     default:
         throw invalid_argument("Code doesn't represent anything");
@@ -82,7 +113,7 @@ int main() {
         char elfCode = roundFileContent.front();
         char myCode = roundFileContent.back();
         PlayItem elfPlay = decodePlay(elfCode);
-        PlayItem myPlay = decodePlay(myCode);
+        PlayItem myPlay = decodePlay(elfPlay.getRiggedItemCode(myCode));
         cout << "Elf plays " << elfPlay.name << " vs " << " I play " << myPlay.name << "\n";
         int score = myPlay.getScore(elfPlay);
         cout << "score: " << score << "\n";
