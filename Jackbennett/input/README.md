5976a79167093deb75cd2da6e023fbf81d70698d
JackBennett <dev@jackben.net>
2022-12-05 18:18:13 +0000

    ⭐Day 2 Part 1

--- Jackbennett/input/README.md
+++ Jackbennett/input/README.md
@@ -8,3 +8,7 @@ Challenge input files cached as generated from the site.
 
 Text file of food values per line item,
 grouped by each elf being separated by an empty line.
+
+### `Rocker_Paper_Scrissors.txt` - Day 2
+
+Text file of `(challenge) (answer)\n`
--- Jackbennett/src/main.rs
+++ Jackbennett/src/main.rs
@@ -1,6 +1,7 @@
 use std::{fs, path};
 
 pub mod elves;
+pub mod rps_tournament;
 
 fn main() {
     let file = fs::read_to_string(path::Path::new("./input/Elf_Calories.txt"))
@@ -44,4 +45,18 @@ fn main() {
             expedition.elves[count].energy()
         );
     }
+
+    let file_rps_tourny = fs::read_to_string(path::Path::new("./input/Rock_Paper_Scissors.txt"))
+        .expect("Missing Day 2 Input File");
+
+    println!();
+
+    let (_, tournament) = rps_tournament::parse_tournament(&file_rps_tourny).unwrap();
+
+    let total_to_plan: usize = tournament.iter().map(|t| t.score()).sum();
+
+    println!(
+        "(Day 2a) Tournament Scores: {} if all goes to plan",
+        total_to_plan
+    );
 }
