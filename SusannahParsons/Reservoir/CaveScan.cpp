dc947f3b3a3446e6779fb829ac4a04d39ae7e01e
Susannah Parsons <bushcraftparsons@gmail.com>
2022-12-16 21:39:17 +0000

    Day 14 part 1 - memory leak?

---
+++ SusannahParsons/Reservoir/CaveScan.cpp
@@ -0,0 +1,139 @@
+#include "CaveScan.h"
+CaveScan::CaveScan(string filename)
+{
+    ifstream inputStream(filename);
+    string fileLine;
+    list<int> xRange;
+    list<int> yRange;
+    while(getline(inputStream, fileLine)){
+        RockStructure rockStructure;
+        string delimiter = " -> ";
+        string locstring = fileLine.substr(0, fileLine.find(delimiter));
+        while(locstring.size()>0){
+            Location* l = rockStructure.getSetLocation(locstring);
+            xRange.insert(xRange.end(), l->x);
+            yRange.insert(yRange.end(), l->y);
+            //Delete old locstring string and first delimiter from fileLine and search for next
+            fileLine.erase(0, locstring.size()+4);
+            locstring = fileLine.substr(0, fileLine.find(delimiter));
+        }
+        rockStructures.insert(rockStructures.end(),rockStructure);
+    }
+    //Add the AIR entry point (500,0)
+    xRange.insert(xRange.end(),entryPoint.x);
+    yRange.insert(yRange.end(),entryPoint.y);
+    xRange.sort();
+    yRange.sort();
+    xMin = xRange.front();
+    xMax = xRange.back();
+    yMin = yRange.front();
+    yMax = yRange.back();
+    cout << "Initialising cave " << "(" << xMin << "," << yMin << ")(" << xMax << "," << yMax << ")\n";
+    for(int xnum = 0; xnum <= (xMax-xMin); xnum++){
+        cave.insert(cave.end(),vector<Tile>());
+    }
+    for(int yVal = yMin; yVal<=yMax; yVal++){
+            cout << "\n";
+        for(int xVal = xMin; xVal<=xMax; xVal++){
+            Location l(xVal, yVal);
+            Tile::TYPE type;
+            if(locationIsRock(l)){
+                type = Tile::ROCK;
+            }else{
+                if(l.x==entryPoint.x && l.y==entryPoint.y){
+                    type = Tile::ENTRY;
+                }else{
+                    type = Tile::AIR;
+                }
+            }
+            Tile t(l,type);
+            cout << t.toString();
+            Location vecLoc(convertCaveLocToVectorLoc(l));
+            cave[vecLoc.x].insert(cave[vecLoc.x].end(),t);
+        }
+    }
+    cout << "\n";
+    dropSand(new Location(convertCaveLocToVectorLoc(entryPoint)),false);
+    toString();
+    cout << sandNumber << " grains of sand landed\n";
+}
+
+bool CaveScan::locationIsRock(Location l)
+{
+    for (auto & rockStructure : rockStructures) {
+        if(rockStructure.hasLocation(l)){
+            return true;
+        }
+    }
+    return false;
+}
+
+Location CaveScan::convertCaveLocToVectorLoc(Location l)
+{
+    return Location(l.x-xMin,l.y-yMin);
+}
+
+Location CaveScan::convertVectorLocToCaveLog(Location l)
+{
+    return Location(l.x+xMin,l.y+yMin);
+}
+
+void CaveScan::toString() const
+{
+    for(int y=0; y < cave[0].size(); y++){
+        cout << "\n";
+        for (int x=0; x < cave.size(); x++){
+            cout << cave[x][y].toString();
+        }
+    }
+    cout << "\n";
+}
+
+void CaveScan::dropSand(Location* sand, bool debug)
+{
+    //Try to drop down
+    if(sand->y+1 > (cave[0].size()-1)){
+        sandFlowsIntoVoid = true;
+    }else if(cave[sand->x][sand->y+1].type == Tile::AIR){
+        if(debug){
+            cout << "Sand drops down " << sand->toString() << "\n";
+        }
+        sand->y++;
+    }//Try to drop diagonal down and left
+    else if(sand->x-1 < 0){
+        sandFlowsIntoVoid = true;
+    }
+    else if(cave[sand->x-1][sand->y+1].type == Tile::AIR){
+        if(debug){
+            cout << "Sand drops left " << sand->toString() << "\n";
+        }
+        sand->x--;
+        sand->y++;
+    }//Try to drop diagonal down and right
+    else if(sand->x+1 > cave.size()-1){
+        sandFlowsIntoVoid = true;
+    }
+    else if(cave[sand->x+1][sand->y+1].type == Tile::AIR){
+        if(debug){
+            cout << "Sand drops right " << sand->toString() << "\n";
+        }
+        sand->x++;
+        sand->y++;
+    }//Can't move, sand comes to rest
+    else{
+        if(debug){
+            cout << "Sand comes to rest " << sand->toString() << "\n";
+        }
+        cave[sand->x][sand->y].type = Tile::SAND;
+        delete sand;
+        Location* sand = new Location(convertCaveLocToVectorLoc(entryPoint));
+        sandNumber++;
+        dropSand(sand,debug);
+    }
+    if(sandNumber>350){
+        sandFlowsIntoVoid = true;
+    }
+    if(!sandFlowsIntoVoid){
+        dropSand(sand,debug);
+    }
+}
---
+++ SusannahParsons/Reservoir/CaveScan.h
@@ -0,0 +1,132 @@
+#ifndef CAVESCAN_H
+#define CAVESCAN_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <list>
+using namespace std;
+struct Location{
+    Location(){};
+    Location(int newx, int newy)
+    :x(newx), y(newy){};
+    Location(const Location& l)
+    :x(l.x),y(l.y){};
+    int x;
+    int y;
+    string toString() const{
+        return "(" + to_string(x) + "," + to_string(y) + ")";
+    };
+    friend bool operator< (const Location& l1, const Location& l2)
+    {
+        if(l1.x==l2.x){
+            return l1.y<l2.y;
+        }else{
+            return l1.x<l2.x;
+        }
+    };
+    friend bool operator== (const Location& l1, const Location& l2){
+        return ((l1.x==l2.x) && (l1.y==l2.y));
+    };
+};
+struct Tile{
+    enum TYPE
+    {
+        SAND,
+        ROCK,
+        ENTRY,
+        AIR
+    };
+    Tile(){};
+    Tile(Location newLoc, TYPE newType)
+    :loc(newLoc), type(newType)
+    {};
+    Location loc;
+    TYPE type;
+    string toString() const
+    {
+        if(type==SAND){
+            return "o";
+        }else if(type==ROCK){
+            return "#";
+        }else if(type==ENTRY){
+            return "+";
+        }else if(type==AIR) {
+            return ".";
+        }
+        return "";
+    };
+    friend bool operator< (const Tile& t1, const Tile& t2)
+    {
+        return t1.loc<t2.loc;
+    };
+};
+struct RockStructure{
+    vector<Location> lines;
+    vector<Location> allRocks;
+    bool hasLocation(Location l){
+        for (auto & rock : allRocks) {
+            if(rock==l){
+                return true;
+            }
+        }
+        return false;
+    };
+    Location* getSetLocation(string locstring){
+        char location[locstring.size() + 1];
+        strcpy(location, locstring.c_str());
+        string x(strtok(location, ","));
+        string y(strtok(NULL, ","));
+        Location* l = new Location(stoi(x),stoi(y));
+        lines.insert(lines.end(),*l);
+        startLine = endLine;
+        endLine = l;
+        if(startLine != nullptr){
+            allRocks.insert(allRocks.end(),*endLine);
+            if(startLine->x==endLine->x){
+                //It's a vertical line
+                int addition = (endLine->y > startLine->y) ? -1 : 1;
+                int newY = endLine->y + addition;
+                while(newY != startLine->y){
+                    allRocks.insert(allRocks.end(),Location(endLine->x, newY));
+                    newY = newY + addition;
+                }
+                allRocks.insert(allRocks.end(),*startLine);
+            }else{
+                //Horizontal line
+                int addition = (endLine->x > startLine->x) ? -1 : 1;
+                int newX = endLine->x + addition;
+                while(newX != startLine->x){
+                    allRocks.insert(allRocks.end(),Location(newX, endLine->y));
+                    newX = newX + addition;
+                }
+                allRocks.insert(allRocks.end(),*startLine);
+            }
+        }
+        return l;
+    };
+private:
+    Location* startLine;
+    Location* endLine = nullptr;
+};
+class CaveScan{
+public:
+    CaveScan(string filename);
+    void toString() const;
+private:
+    void dropSand(Location* sand, bool debug);
+    bool sandFlowsIntoVoid = false;
+    vector<RockStructure> rockStructures;
+    vector<vector<Tile>> cave;
+    int xMin;
+    int xMax;
+    int yMin;
+    int yMax;
+    int sandNumber = 0;
+    Location entryPoint = Location(500,0);
+    bool locationIsRock(Location l);
+    Location convertCaveLocToVectorLoc(Location l);
+    Location convertVectorLocToCaveLog(Location l);
+};
+#endif // CAVESCAN_H
