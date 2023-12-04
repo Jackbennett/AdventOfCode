b8985e493055c184d940bc353b8d635c67f02bcb
Susannah Parsons <bushcraftparsons@gmail.com>
2023-12-04 02:28:44 +0000

    Day 3 part 1

--- SusannahParsons/2023/AoC/main.go
+++ SusannahParsons/2023/AoC/main.go
@@ -3,19 +3,29 @@
 package main
 
 import (
-	cubes "AoC/day2"
+	engine "AoC/day3"
 	"fmt"
 )
 
 func main() {
-
-	var data = returnFileContentsArray("day2/input.txt")
-	var games cubes.Games
-	for _, s := range data {
-		games.ParseGame(s, 12, 14, 13)
+	var data = returnFileContentsArray("day3/input.txt")
+	//Day 3
+	var engine engine.Engine
+	for i, s := range data {
+		engine.AddLine(s, i)
 	}
-	fmt.Print(games.SumPossibleIds, " total.\n")
-	fmt.Print(games.SumPower, " sum powers\n")
+	engine.FindPartNumbers()
+	fmt.Print(engine.TotalPartNumber, " total.\n")
+
+	//Day 2
+	// var games cubes.Games
+	// for _, s := range data {
+	// 	games.ParseGame(s, 12, 14, 13)
+	// }
+	// fmt.Print(games.SumPossibleIds, " total.\n")
+	// fmt.Print(games.SumPower, " sum powers\n")
+
+	//Day 1
 	// var treb1 trebuchet.Trebuchet
 	// for _, s := range data {
 	// 	treb1.AddCalibration(s)
