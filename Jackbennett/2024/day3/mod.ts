7ad46eb93b1c5cdb9fa3eec006bef972dd4d2e10
Jack <code@jackben.net>
2024-12-03 07:55:24 +0000

    day 3 part 2

--- Jackbennett/2024/day3/mod.ts
+++ Jackbennett/2024/day3/mod.ts
@@ -1,15 +1,15 @@
 import { assertEquals } from "jsr:@std/assert";
 
-const parse = (f:string) => {
+const parse = (f:string, pattern: RegExp) => {
     const file = Deno.readTextFileSync(f)
-    const matches = file.matchAll(/mul\((\d+),(\d+)\)/g)
+    const matches = file.matchAll(pattern)
     return matches
 }
 
 const int = (n: string) => parseInt(n, 10) 
 
 export function part1(){
-    const data = parse('./input.txt')
+    const data = parse('./input.txt', /mul\((\d+),(\d+)\)/g)
     let sum = 0
     for( const [_ins, a, b ] of data) {
         sum += int(a) * int(b)
@@ -21,5 +21,30 @@ Deno.test('Day 3 parts 1', () => {
     assertEquals(part1(), 164730528)
 })
 
+export function part2() {
+    const data = parse('./input.txt', /(mul\((\d+),(\d+)\)|(do|don't)\(\))/g)
+    let process = true
+    let sum = 0
+    for (const [ins, ...args] of data){
+        switch(ins.split('(').at(0)) {
+            case 'mul': {
+                    const [_pattern, a,b] = args
+                    sum += process ? int(a) * int(b) : 0
+                    break;
+                }
+            case 'do':
+                process = true
+                break;
+            case "don't":
+                process = false
+                break;
+        }
+    }
+    return sum
+}
 
-console.log(part1())
+Deno.test('Day 3 part 2', () => { 
+    assertEquals(part2(), 70478672)
+})
+
+console.log(part2())
