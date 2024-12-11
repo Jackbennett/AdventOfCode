02ee7cf37c36512fe83edfb47d8821ac3d111468
Jack <code@jackben.net>
2024-12-11 00:24:00 +0000

    day 7 part 2 first try. takes a couple of seconds to run.

--- Jackbennett/2024/day7/mod.ts
+++ Jackbennett/2024/day7/mod.ts
@@ -81,31 +81,57 @@ const part1 = (d: ReturnType<typeof parse>) => {
     return correct.map(x => x.answer).reduce(sum, 0)
 }
 
+// Deno.test('day 7 example', () => {
+//     const d = parse(example)
+//     console.log(d)
+//     const a = part1(d)
+//     console.log(a)
+//     assertEquals(a, 3749)
+// })
+
+// Deno.test('day 7 mul first edge case', () => {
+//     const d = parse(`10: 3 3 1`)
+//     console.log(d)
+//     const a = part1(d)
+//     console.log(a)
+//     assertEquals(a, 10)
+// })
+
+// Deno.test('day 7 part 1', () => {
+//     const d = parse(Deno.readTextFileSync("./input.txt"))
+//     const a = part1(d)
+//     console.log(a)
+//     assertGreater(a, 247970890361)
+//     assertEquals(a, 5512534574980)
+// })
+
+const part2 = (d: ReturnType<typeof parse>) => {
+    const ops: Operators = {
+        '+': (a,b) => a + b,
+        '*': (a,b) => a * b,
+        '||': (a,b) => parseInt(`${a}${b}`)
+    }
+
+    const answers = d.map(x => checkPossible(x, ops))
+    const correct = answers.filter(x => Array.isArray(x.equation))
+    const incorrect = answers.filter(x => x.equation === undefined)
+    const _removed_high = incorrect.filter(x => x.input.reduce( (t,v) => v===1 ? t + v : t * v, 0 ) < x.answer) // largest op can't get big enough
+    const _removed_low = incorrect.filter(x => x.input.reduce(sum, 0) > x.answer) // smallest op can't be low enough
+    return correct.map(x => x.answer).reduce(sum, 0)
+}
+
 Deno.test('day 7 example', () => {
     const d = parse(example)
     console.log(d)
-    const a = part1(d)
+    const a = part2(d)
     console.log(a)
-    assertEquals(a, 3749)
+    assertEquals(a, 11387)
 })
 
-Deno.test('day 7 mul first edge case', () => {
-    const d = parse(`10: 3 3 1`)
-    console.log(d)
-    const a = part1(d)
-    console.log(a)
-    assertEquals(a, 10)
-})
-
-Deno.test('day 7 part 1', () => {
+Deno.test('day 7 part 2', () => {
     const d = parse(Deno.readTextFileSync("./input.txt"))
-    const a = part1(d)
+    console.log(d)
+    const a = part2(d)
     console.log(a)
-    assertGreater(a, 247970890361)
-    assertEquals(a, 5512534574980)
+    assertEquals(a, 328790210468594)
 })
-
-
-// const part2 = (d: ReturnType<typeof parse>) => {
-
-// }
