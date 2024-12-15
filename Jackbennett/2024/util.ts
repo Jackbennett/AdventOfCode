0ad83d43761264754dd93660adb4b56d499d3498
Jack <code@jackben.net>
2024-12-15 18:11:45 +0000

    Day 15

--- Jackbennett/2024/util.ts
+++ Jackbennett/2024/util.ts
@@ -6,3 +6,29 @@ export const parse = (f:string): string[] => {
 }
 
 export const toSplitNumber = (list: string, on= ' ') => list.split(on).filter(d => !!d).map(d => parseInt(d, 10))
+
+export class Coordinate {
+    constructor(public x: number, public y: number) {}
+    add({x = this.x, y = this.y}: Coordinate | {x?: number, y: number} | {x: number, y?: number}) {
+        return new Coordinate(this.x + x, this.y + y)
+    }
+    is(to: Coordinate): boolean {
+        return this.compare(to) === 0
+    }
+    compare(to: Coordinate) {
+        if(this.x > to.x || this.y > to.y) return 1
+        if(this.x < to.x || this.y < to.y) return -1
+        if(this.x === to.x && this.y === to.y) return 0
+        if(this.x > to.x && this.y > to.y) return 1
+        if(this.x < to.x && this.y < to.y) return -1
+        return -1
+    }
+}
+
+export class Robot {
+    constructor(public position: Coordinate, public speed: Coordinate, public serial: number) {}
+    tick() {
+        this.position.x += this.speed.x
+        this.position.y += this.speed.y
+    }
+}
